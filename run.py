5065fa4cbfefe4142f1608e7a5ac9e743a1846d9
Lucas Baudin <lucas.baudin@ens.fr>
2015-03-12 14:04:50 +0000

    Debug facilities

--- run.py
+++ run.py
@@ -6,4 +6,8 @@ import os
 # Start development web server
 if __name__=='__main__':
 	app = create_app()
-	app.run(host='0.0.0.0', port=5000, debug=int(os.getenv("DEBUG", False)))
+	debug = os.getenv("DEBUG", False)
+	if debug:
+		app.run(host='localhost', port=5000, debug=True)
+	else:
+		app.run(host='0.0.0.0', port=5000)
